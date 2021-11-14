ad63a2c3ae99cba45940b291aa359e4541300606
Chris S <chrisasmall+github@gmail.com>
2021-11-14 21:50:15 +0000

    Use Gin for HTTP server

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+// go get -u github.com/gin-gonic/gin
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("💻 -- boiler plate for VS code setup")
+	fmt.Println("💻 -- Running on localhost:8080 using Gin.")
+	fmt.Println("💻 -- try http://localhost:8080/ping")
+
+	// Default With the Logger and Recovery middleware already attached
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
 	// add test framework
 }
